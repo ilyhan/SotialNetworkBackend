0394adaea56b23d1ee5002bfed9553472d5db394
Илья Нестеров <88728202+ilyhan@users.noreply.github.com>
2024-10-14 15:52:29 +0300

    Первоначальная настройка

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+const cors = require('cors');
+
+app.use(express.json());
+app.use(cors());
+
+app.listen(3001, () => {
+    console.log("Server is running on http://localhost:3001");
+});
