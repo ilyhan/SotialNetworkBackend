8938edd623eedc32d15e459736d091a24ab583ff
Илья Нестеров <88728202+ilyhan@users.noreply.github.com>
2024-10-17 22:38:00 +0300

    посты (без файлов)

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -9,7 +9,7 @@ const generateAccessToken = (id) => {
         id,
     }
 
-    return jwt.sign(payload, secret, { expiresIn: "220s" });
+    return jwt.sign(payload, secret, { expiresIn: "1d" });
 }
 
 class UserController {
@@ -74,7 +74,71 @@ class UserController {
             const { id } = req.user;
             res.status(200).json(id);
         } catch (e) {
-            return res.status(400).json({ message: "Произошла ошибка авторизации пользователя", error: e.message });
+            return res.status(400).json({ message: "Произошла непредвиденная ошибка", error: e.message });
+        }
+    }
+
+    async createPost(req, res) {
+        try {
+            const {
+                content,
+            } = req.body;
+            const { id } = req.user;
+
+            const result = await db.query('INSERT INTO posts (user_id, content) values ($1, $2) RETURNING *', [id, content]);
+
+            if (!result.rows.length) {
+                return res.status(400).json({ message: "Ошибка записи данных" });
+            }
+            return res.json(result);
+        } catch (e) {
+            return res.status(400).json({ message: "Произошла непредвиденная ошибка", error: e.message });
+        }
+    }
+
+    async getAllPosts(req, res) {
+        try {
+            const { id } = req.user;
+
+            const result = await db.query(`
+                SELECT 
+                    p.id, 
+                    p.user_id,
+                    p.content,
+                    p.media_content, 
+                    p.likes_count,
+                    p.created_at,
+                    EXISTS (SELECT 1 FROM likes l WHERE l.post_id = p.id AND l.user_id = $1) AS liked
+                FROM 
+                    posts p
+                ORDER BY 
+                    p.created_at DESC
+            `, [id]);
+
+            res.json(result.rows);
+        } catch (e) {
+            return res.status(400).json({ message: "Произошла непредвиденная ошибка", error: e.message });
+        }
+    }
+
+    async likes(req, res) {
+        const { post_id } = req.body;
+        const { id: user_id } = req.user;
+
+        try {
+            const result = await db.query('SELECT * FROM likes WHERE user_id = $1 AND post_id = $2', [user_id, post_id]);
+
+            if (result.rows.length > 0) {
+                await db.query('DELETE FROM likes WHERE user_id = $1 AND post_id = $2', [user_id, post_id]);
+                await db.query('UPDATE posts SET likes_count = likes_count - 1 WHERE id = $1', [post_id]);
+                return res.json({ message: 'Лайк удален' });
+            } else {
+                await db.query('INSERT INTO likes (user_id, post_id) VALUES ($1, $2)', [user_id, post_id]);
+                await db.query('UPDATE posts SET likes_count = likes_count + 1 WHERE id = $1', [post_id]);
+                return res.json({ message: 'Пост лайкнут' });
+            }
+        } catch (e) {
+            return res.status(500).json({ message: "Произошла непредвиденная ошибка", error: e.message });
         }
     }
 }
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -9,11 +9,12 @@ router.post('/user', [
     check('last_name', "Фамилия пользователя не может быть пуста").notEmpty(),
     check('username', "Ник пользователя не может быть пуст").notEmpty(),
     check('email', "Некорректные данные в поле почта").isEmail(),
-    check('password', "Пароль должен быть больше 5 и меньше 15 символов").isLength({min: 5, max: 15}),
+    check('password', "Пароль должен быть больше 5 и меньше 15 символов").isLength({ min: 5, max: 15 }),
 ], userController.createUser);
 router.post('/login', userController.login);
-router.post('/refresh', authorization, userController.refresh);
-// router.get('/user/:nickname', userController.getUser);
-// router.get('/message', userController.getMessages);
+router.get('/refresh', authorization, userController.refresh);
+router.post('/newpost', authorization, userController.createPost);
+router.get('/posts', authorization, userController.getAllPosts);
+router.post('/post/like', authorization, userController.likes);
 
 module.exports = router;
