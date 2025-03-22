a1adbf9be97f60f780f82212f8b35dbb9afda0f2
Илья Нестеров <88728202+ilyhan@users.noreply.github.com>
2025-03-23 01:00:24 +0300

    test

--- middlewares/authorization.js
+++ middlewares/authorization.js
@@ -3,6 +3,8 @@ const { secret } = require('../config');
 
 function authorization(req, res, next) {
     const token = req.cookies.token;
+    const clientIP = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    console.log('Client IP:', clientIP);
 
     if(!token) {
         return res.status(401).json({message: 'Пользователь не авторизован'});
