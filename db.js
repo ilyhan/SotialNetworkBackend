a83e406adb0f91ed0228515be9f3891c5ade54b0
Илья Нестеров <88728202+ilyhan@users.noreply.github.com>
2025-03-30 12:05:52 +0300

    new database

--- db.js
+++ db.js
@@ -6,7 +6,7 @@ const pool = new Pool({
     password: process.env.DB_PASSWORD,
     host: process.env.DB_HOST, 
     port: 5432, 
-    database: "dbpostnest",
+    database: "PostNest",
     ssl: {
         rejectUnauthorized: false 
     }
